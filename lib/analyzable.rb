32f6226f07650ae5c85e75fd7054991207637802
Karim Tarek <karimmtarek@gmail.com>
2016-03-07 21:01:57 -0500

    Add count_by_brand method to Analyzable, and pass the tests

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -2,4 +2,15 @@ module Analyzable
   def self.average_price(products)
     (products.map { |p| p.price.to_f }.reduce(:+) / products.size).round(2)
   end
+
+  def self.count_by_brand(products)
+    brands_count = {}
+    brands = products.map(&:brand).uniq
+
+    brands.each do |brand|
+      brands_count[brand] = products.select(&:brand).size
+    end
+
+    brands_count
+  end
 end
